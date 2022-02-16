75723d1d8acc1c615d209f454dd2a3d0fe411c60
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-02-16 22:58:51 +0400

    [Add] Project init(Client)
    - create-react-app 실행
    - favicon 설정 및 기본 meta 정보 작성
    - Style reset 적용
    - Font-family 설정
    - Prettier 및 EsLint 설정
    - .gitignore 설정
    - react-router-dom 설치

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <select>
+        <option>Hi</option>
+      </select>
+      <input type="date" />
+    </div>
+  );
+}
+
+export default App;
