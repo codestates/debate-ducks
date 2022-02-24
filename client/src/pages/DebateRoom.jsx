708a5dbd3fb52d4c337c5d3ea675ebc045b60c98
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-02-25 00:35:12 +0400

    [Add] 실시간 채팅 기본 연결

--- client/src/pages/DebateRoom.jsx
+++ client/src/pages/DebateRoom.jsx
@@ -2,6 +2,7 @@ import io from "socket.io-client";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Chat from "../components/debate_room/Chat";
+import Video from "../components/debate_room/Video";
 
 export default function DebateRoom() {
   const { debateId } = useParams();
@@ -11,10 +12,13 @@ export default function DebateRoom() {
     setSocket(io(`${process.env.REACT_APP_API_URL}`));
   }, []);
 
+  //! 임시 변수
+  const isDebating = false;
+
   return (
     <div>
       <h1>-DebateRoom-</h1>
-      {Object.keys(socket).length === 0 ? <div>Loading</div> : <Chat socket={socket} debateId={debateId} />}
+      {Object.keys(socket).length === 0 ? <div>Loading</div> : isDebating ? <Chat socket={socket} debateId={debateId} /> : <Video socket={socket} debateId={debateId} />}
     </div>
   );
 }
