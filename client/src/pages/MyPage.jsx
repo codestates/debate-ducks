8b332d5b211b64dcdb790f218b1d0b3ccc75c9ef
jennykim <jennykim97@gmail.com>
2022-03-02 09:32:59 +0900

    [Add] MyPage

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -1,7 +1,7 @@
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { FiEdit } from "react-icons/fi";
 import { AiOutlineCheck } from "react-icons/ai";
-//import { MdOutlineCancel } from "react-icons/md";
+import { MdOutlineCancel } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import ConfirmModal from "../components/modal/ConfirmModal";
 import { useSelector, useDispatch } from "react-redux";
@@ -17,13 +17,13 @@ export default function MyPage() {
   const navigate = useNavigate();
 
   const [isEditUsername, setIsEditUsername] = useState(false);
-  const [inputUsername, setInputUsername] = useState("userInfo?.name");
+  const [inputUsername, setInputUsername] = useState("");
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
-  const [profilePic, setProfilePic] = useState("userInfo?.profile");
 
   const userInfo = useSelector((state) => state.user.data);
 
   const [localUserInfo, setLocalUserInfo] = useState({});
+  //const [preview, setPreview] = useState("");
 
   const changeUsernameInput = (e) => {
     setInputUsername(e.target.value);
@@ -36,8 +36,24 @@ export default function MyPage() {
   console.log("userInfo", userInfo);
   console.log("localUserInfo", localUserInfo);
 
+  // useEffect(() => {
+  //   if (profilePic) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setPreview(reader.result);
+  //     };
+  //     reader.readAsDataURL(profilePic);
+  //   } else {
+  //     setPreview(null);
+  //   }
+  // }, [profilePic]);
+
   const onEditUsername = () => {
     setIsEditUsername(true);
+  };
+
+  const cancelEditUsername = () => {
+    setIsEditUsername(false);
   };
 
   const [isUsernameValid, setIsUsernameValid] = useState(true);
@@ -54,6 +70,7 @@ export default function MyPage() {
       .then((res) => {
         dispatch(setUserInfo(res.data.data));
         setLocalUserInfo((state) => ({ ...state, ...res.data.data }));
+        window.location.reload(); //navigate('/mypage')와의 차이점?
       })
       .catch((err) => {
         alert(err);
@@ -70,48 +87,93 @@ export default function MyPage() {
   // Get Kakao Profile automatically from server when logged in (this is re-rendered at userInfo.profile change)
   // ! 쿠키로 userInfo 들어오면 get 안에 바꿔야함
 
+  const [image, setImage] = useState({});
+
   const fileChangeHandler = (e) => {
-    setProfilePic(e.target.file);
+    // setProfilePic(e.target.file);
+    //e.preventDefault();
+    console.log(e.target.files["0"]);
+    setImage(e.target.files["0"]);
+    // console.log(e.target.value);
+    // let reader = new FileReader();
+    // reader.onloadend = () => {
+    //   const file = reader.result;
+    //   if (file) {
+    //     setProfilePic(file.toString());
+    //   }
+    // };
+    // if (e.target.file) {
+    //   reader.readAsDataURL(e.target.file);
+    // }
   };
-  // When 'Change Profile Picture' button is clicked, and file is uploaded from user, client shows image on screen
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    //file uploaded from user
+
+  const onSubmitHandler = () => {
+    //console.log("시작이미지", image);
     let formData = new FormData();
 
     const config = {
       header: { "content-type": "multipart/form-data" },
     };
-    formData.append("img", formData);
+    formData.set("img", image);
+    //console.log("두번째이미지입", image);
+    //console.log("폼데이터", e.target.files["0"]);
 
-    axios.post(`${process.env.REACT_APP_API_URL}/profile`, formData, config).then((response) => {
-      if (response.data.success) {
-        console.log(response.data);
-        alert("saved");
-      } else {
-        alert("Failed to save profile.");
-      }
-    });
-    setProfilePic(formData);
+    if (image) {
+      axios
+        .patch(`${process.env.REACT_APP_API_URL}/user/${localUserInfo?.id}/img`, formData, config)
+        .then((res) => {
+          dispatch(setUserInfo(res.data.data));
+          setLocalUserInfo((state) => ({ ...state, ...res.data.data }));
+          window.location.reload();
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  };
+
+  const onCheckEnter = (e) => {
+    if (e.key === "Enter") {
+      saveUsername();
+    }
+  };
+
+  const deleteAccount = async () => {
+    signoutHandler();
+    await axios
+      .delete(`${process.env.REACT_APP_API_URL}/user/${localUserInfo?.id}`, {
+        withCredentials: true,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const signoutHandler = () => {
     axios
       .post(`${process.env.REACT_APP_API_URL}/signout`, {}, { withCredentials: true })
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        dispatch(setUserInfo({}));
+        setLocalUserInfo((state) => ({ ...state, ...{} }));
+        navigate("/");
+      })
       .catch((err) => console.log(err));
   };
 
   return (
     <div className="flex justify-center items-center m-112">
-      <div>Test: {localUserInfo?.name}</div>
+      {/* <div>Test: {localUserInfo?.name}</div> */}
       {/* 사진왼쪽 정보오른쪽 구분 */}
       <div className="flex flex-row">
         <div className="flex flex-col w-274 h-410 rounded-20 mr-20">
-          {/* 여기서 삼항연산자 ---> {profilePic ? "양식 안에 profilePic 들어가게" : "기본 오리 사진" }*/}
-          <div className="bg-strawduck bg-cover w-274 h-274 mb-12 rounded-full border-solid border-ducks-orange-ff9425 border-4">{profilePic}</div>
-          <form onSubmit={onSubmitHandler}>Change profile : </form>
-          <input type="file" onChange={fileChangeHandler} />{" "}
+          <img src={userInfo?.profile} className="rounded-full w-274 h-274 object-cover object-center" />
+          <form type="file" name="img" encType="multipart/form-data">
+            Change profile :
+            <input type="file" name="img" onChange={fileChangeHandler} />
+          </form>
+          <button onClick={onSubmitHandler}>Save</button>
         </div>
 
         {/* BIG Right 정보오른쪽 container */}
@@ -119,61 +181,25 @@ export default function MyPage() {
           {/* BIG Right 의 flex-row Username, 수정삭제, email정보*/}
           <div className="flex flex-col">
             <div className="flex flex-row">
-              {!isEditUsername ? <div className="text-24 font-poppins">{localUserInfo?.name}</div> : <input className="ml-12" onChange={changeUsernameInput} value={inputUsername} />}
-              {!isEditUsername ? <FiEdit className="m-12" onClick={onEditUsername} /> : <AiOutlineCheck className="m-12" onClick={saveUsername} />}
-              {isUsernameValid ? <div className="mb-10" /> : <div className="text-xs text-center mb-10">Username must be longer than ?? words and must not contain ????</div>}
+              {!isEditUsername ? (
+                <div className="text-24 font-poppins">{localUserInfo?.name}</div>
+              ) : (
+                <input className="ml-12" onChange={changeUsernameInput} value={inputUsername} onKeyPress={onCheckEnter} />
+              )}
+              {!isEditUsername ? (
+                <FiEdit className="m-12" onClick={onEditUsername} />
+              ) : (
+                <>
+                  <AiOutlineCheck className="m-12" onClick={saveUsername} />
+                  <MdOutlineCancel className="m-12" onClick={cancelEditUsername} />
+                </>
+              )}
+              {isUsernameValid ? <div className="mb-10" /> : <div className="text-xs text-center mb-10">Username must be between 1 to 36 words.</div>}
             </div>
             <div className="flex flex-row mt-12">
               <RiKakaoTalkFill className="text-24 mr-12 mb-20 text-black-00000" />
               <div className="flex text-18 font-poppins">{localUserInfo?.email}</div>
             </div>
-          </div>
-          {/* BIG Right 의 첫번째 orange box*/}
-          <div className="flex flex-row border border-solid border-ducks-orange-ff9425 rounded-12 p-18 mt-12 mb-18">
-            {/* 그 안의 왼쪽 ranking,debates,votes */}
-            {/* <div className="flex flex-col">
-              <div className="flex flex-row">
-                <span className="flex text-ducks-orange-ff9425 w-140 m-12">Ranking</span>
-                <span className="flex font-bold w-140 m-12">{userInfo.ranking}</span>
-              </div>
-              <div className="flex flex-row">
-                <span className="flex text-ducks-orange-ff9425 w-140 m-12">Debates</span>
-                <span className="flex font-bold w-140 m-12">{userInfo.debates}</span>
-              </div>
-              <div className="flex flex-row">
-                <span className="flex text-ducks-orange-ff9425 w-140 m-12">Votes</span>
-                <span className="flex font-bold w-140 m-12">{userInfo.votes}</span>
-              </div>
-            </div> */}
-            {/* 그 안의 오른쪽 points, winrate, interestedin */}
-            {/* <div className="flex flex-col">
-              <div className="flex flex-row">
-                <span className="flex text-ducks-orange-ff9425 w-140 m-12">Points</span>
-                <span className="flex font-bold w-140 m-12">{userInfo.points}</span>
-              </div>
-              <div className="flex flex-row">
-                <span className="flex text-ducks-orange-ff9425 w-140 m-12">Win Rate</span>
-                <span className="flex font-bold w-140 m-12">{userInfo.winRate}%</span>
-              </div>
-              <div className="flex flex-row">
-                <span className="flex text-ducks-orange-ff9425 w-140 m-12">Interested In</span>
-                <span className="flex w-140 m-12">{userInfo.interestedIn}</span>
-              </div>
-            </div> */}
-          </div>
-
-          {/* BIG Right의 두번째 orange box */}
-          <div className="flex flex-row border border-solid border-ducks-orange-ff9425 rounded-12 p-18 mt-12 mb-18">
-            {/* 그 안의 왼쪽 columns */}
-            {/* <div className="flex flex-row">
-              <span className="flex text-ducks-orange-ff9425 w-140 m-12">Columns</span>
-              <span className="flex font-bold w-140 m-12">{userInfo.columns}</span>
-            </div> */}
-            {/* 그 안의 오른쪽 opinions */}
-            {/* <div className="flex flex-row">
-              <span className="flex text-ducks-orange-ff9425 w-140 m-12">Opinions</span>
-              <span className="flex font-bold w-140 m-12">{userInfo.opinions}</span>
-            </div> */}
           </div>
           {/* BIG Right의 show activities, delete account */}
           <div className="flex flex-col mt-24 mb-30">
@@ -183,7 +209,15 @@ export default function MyPage() {
             <button className="flex text-ducks-gray-666 underline" onClick={confirm}>
               Delete Account
             </button>
-            {isConfirmOpen ? <ConfirmModal /> : null}
+            {isConfirmOpen ? (
+              <ConfirmModal
+                content={{ title: "Alert!", text: "Are you sure you want delete your account?", left: "NO", right: "YES" }}
+                cancelCallback={() => {
+                  setIsConfirmOpen(false);
+                }}
+                confirmCallback={deleteAccount}
+              />
+            ) : null}
           </div>
         </div>
       </div>
