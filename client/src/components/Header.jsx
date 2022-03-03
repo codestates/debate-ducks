d68509e284a7f4eaee2ec3b71cf4c4c02dca9079
strawberryoolongtea <strawberrycream@kakao.com>
2022-03-04 02:41:47 +0900

    [Fix] 프로필 클릭시 마이페이지 & 로그아웃 구현

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { OrangeBtn as SignInBtn } from "./btn/BaseBtn";
-import UserInfoModal from "./modal/UserInfoModal";
 import { HiBell } from "react-icons/hi";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
@@ -28,6 +27,13 @@ export default function Header() {
 
   const listItemStyle = "mx-20 px-12 py-12 cursor-pointer";
 
+  const handleSignout = () => {
+    axios.post(`${process.env.REACT_APP_API_URL}/signout`, {}, { withCredentials: true }).then(() => {
+      dispatch(setUserInfo({}));
+      navigate("/");
+    });
+  };
+
   return /^\/forum\/debateroom\/\d/i.test(location.pathname) ? null : (
     <div className="flex justify-between items-center h-60 px-20 border-b border-solid border-ducks-gray-eee">
       <div className="flex items-center cursor-pointer" onClick={() => navigate("/")}>
@@ -53,7 +59,22 @@ export default function Header() {
           <div className="flex items-center">
             <HiBell className="text-24 mr-20 text-ducks-orange-ff9425" />
             <img src={userInfo.profile} className="rounded-full w-32 h-32 bg-ducks-orange-ff9425 relative" onClick={toggleUserInfoModal}></img>
-            {isUserModalOn ? <UserInfoModal content={userInfo}></UserInfoModal> : null}
+            {isUserModalOn ? (
+              <div className="z-50 absolute top-48 right-20 bg-white rounded-12 border border-solid border-ducks-orange-ff9425 text-14 text-ducks-orange-ff9425 overflow-hidden cursor-pointer">
+                <div
+                  onClick={() => {
+                    setIsUserModalOn(!isUserModalOn);
+                    navigate("/mypage");
+                  }}
+                  className="p-3 border-b border-solid border-ducks-orange-ff9425 hover:text-white hover:bg-ducks-orange-ff9425"
+                >
+                  My Page
+                </div>
+                <div onClick={handleSignout} className="p-3 hover:text-white hover:bg-ducks-orange-ff9425">
+                  Sign Out
+                </div>
+              </div>
+            ) : null}
           </div>
         ) : (
           <SignInBtn
