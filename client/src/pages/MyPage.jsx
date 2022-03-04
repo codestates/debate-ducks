b1a0cc5b197c9f78421be1f76e61ceac3500c380
jennykim <jennykim97@gmail.com>
2022-03-04 15:31:21 +0900

    [Update] MyPage 수정

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -9,8 +9,7 @@ import { useState } from "react";
 import getByteLength from "../utils/getByteLength";
 import axios from "axios";
 import { setUserInfo } from "../redux/modules/user";
-// import { getUserInfo, postUserInfo } from "../redux/modules/user";
-// import useGetAsync from "../hooks/useGetAsync";
+import { OrangeBtn } from "../components/btn/BaseBtn";
 
 export default function MyPage() {
   const dispatch = useDispatch();
@@ -20,35 +19,14 @@ export default function MyPage() {
   const [inputUsername, setInputUsername] = useState("");
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
 
+  // 리덕스 리렌더링을 위한 작업
   const userInfo = useSelector((state) => state.user.data);
-
-  //const [userInfo, setLocalUserInfo] = useState({});
-  //const [profileCheck, setProfileCheck] = useState(true);
-  //const [preview, setPreview] = useState("");
+  const userName = useSelector((state) => state.user.data.name);
+  const userProfile = useSelector((state) => state.user.data.profile);
 
   const changeUsernameInput = (e) => {
     setInputUsername(e.target.value);
   };
-
-  // useEffect(() => {
-  //   setLocalUserInfo((state) => ({ ...state, ...userInfo }));
-  //   console.log("유저인포", userInfo);
-  // }, [userInfo.name, profileCheck]);
-
-  // console.log("userInfo", userInfo);
-  // console.log("localUserInfo", localUserInfo);
-
-  // useEffect(() => {
-  //   if (profilePic) {
-  //     const reader = new FileReader();
-  //     reader.onloadend = () => {
-  //       setPreview(reader.result);
-  //     };
-  //     reader.readAsDataURL(profilePic);
-  //   } else {
-  //     setPreview(null);
-  //   }
-  // }, [profilePic]);
 
   const onEditUsername = () => {
     setIsEditUsername(true);
@@ -71,12 +49,11 @@ export default function MyPage() {
       })
       .then((res) => {
         dispatch(setUserInfo(res.data.data));
-        //setLocalUserInfo((state) => ({ ...state, ...res.data.data }));
-        window.location.reload(); //navigate('/mypage')와의 차이점?
       })
       .catch((err) => {
         alert(err);
       });
+    setIsEditUsername(false);
   };
 
   const confirm = () => {
@@ -86,55 +63,33 @@ export default function MyPage() {
       setIsConfirmOpen(false);
     }
   };
-  // Get Kakao Profile automatically from server when logged in (this is re-rendered at userInfo.profile change)
-  // ! 쿠키로 userInfo 들어오면 get 안에 바꿔야함
 
   const [image, setImage] = useState({});
 
   const fileChangeHandler = (e) => {
-    // setProfilePic(e.target.file);
-    //e.preventDefault();
-    console.log(e.target.files["0"]);
+    //console.log(e.target.files["0"]);
     setImage(e.target.files["0"]);
-    // console.log(e.target.value);
-    // let reader = new FileReader();
-    // reader.onloadend = () => {
-    //   const file = reader.result;
-    //   if (file) {
-    //     setProfilePic(file.toString());
-    //   }
-    // };
-    // if (e.target.file) {
-    //   reader.readAsDataURL(e.target.file);
-    // }
   };
 
   const onSubmitHandler = () => {
-    //console.log("시작이미지", image);
     let formData = new FormData();
 
     const config = {
       header: { "content-type": "multipart/form-data" },
     };
     formData.set("img", image);
-    //console.log("두번째이미지입", image);
-    //console.log("폼데이터", e.target.files["0"]);
 
     if (image) {
       axios
         .patch(`${process.env.REACT_APP_API_URL}/user/${userInfo?.id}/img`, formData, config)
         .then((res) => {
           dispatch(setUserInfo(res.data.data));
-          // setLocalUserInfo((state) => ({ ...state, ...res.data.data }));
-          window.location.reload();
-          // setProfileCheck(!profileCheck);
         })
         .catch((err) => {
           alert(err);
         });
     }
   };
-  //console.log("프로필체크", profileCheck);
 
   const onCheckEnter = (e) => {
     if (e.key === "Enter") {
@@ -143,53 +98,48 @@ export default function MyPage() {
   };
 
   const deleteAccount = async () => {
-    signoutHandler();
+    //signoutHandler();
     await axios
       .delete(`${process.env.REACT_APP_API_URL}/user/${userInfo?.id}`, {
         withCredentials: true,
       })
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        dispatch(setUserInfo({}));
+        navigate("/");
+      })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  const signoutHandler = () => {
-    axios
-      .post(`${process.env.REACT_APP_API_URL}/signout`, {}, { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-        dispatch(setUserInfo({}));
-        //setLocalUserInfo((state) => ({ ...state, ...{} }));
-        navigate("/");
-      })
-      .catch((err) => console.log(err));
-  };
+  // const signoutHandler = () => {
+  //   axios
+  //     .post(`${process.env.REACT_APP_API_URL}/signout`, {}, { withCredentials: true })
+  //     .then((res) => {
+  //       console.log(res);
+  //dispatch(setUserInfo({}));
+  //navigate("/");
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
   return (
     <div className="flex justify-center items-center m-112">
-      {/* <div>Test: {localUserInfo?.name}</div> */}
-      {/* 사진왼쪽 정보오른쪽 구분 */}
       <div className="flex flex-row">
         <div className="flex flex-col w-274 h-410 rounded-20 mr-20">
-          <img src={userInfo?.profile} className="rounded-full w-274 h-274 object-cover object-center" />
+          <img src={userProfile} className="rounded-full w-274 h-274 object-cover object-center" />
           <form type="file" name="img" encType="multipart/form-data">
             Change profile :
             <input type="file" name="img" onChange={fileChangeHandler} />
           </form>
-          <button onClick={onSubmitHandler}>Save</button>
+          <OrangeBtn callback={onSubmitHandler} text="Save"></OrangeBtn>
         </div>
 
-        {/* BIG Right 정보오른쪽 container */}
         <div className="flex flex-col h-410 rounded-12 ml-20">
-          {/* BIG Right 의 flex-row Username, 수정삭제, email정보*/}
           <div className="flex flex-col">
             <div className="flex flex-row">
-              {!isEditUsername ? (
-                <div className="text-24 font-poppins">{userInfo?.name}</div>
-              ) : (
-                <input className="ml-12" onChange={changeUsernameInput} value={inputUsername} onKeyPress={onCheckEnter} />
-              )}
+              {!isEditUsername ? <div className="text-24 font-poppins">{userName}</div> : <input className="ml-12" onChange={changeUsernameInput} value={inputUsername} onKeyPress={onCheckEnter} />}
               {!isEditUsername ? (
                 <FiEdit className="m-12" onClick={onEditUsername} />
               ) : (
@@ -205,7 +155,6 @@ export default function MyPage() {
               <div className="flex text-18 font-poppins">{userInfo?.email}</div>
             </div>
           </div>
-          {/* BIG Right의 show activities, delete account */}
           <div className="flex flex-col mt-24 mb-30">
             <button className="flex text-ducks-gray-666 underline" onClick={() => navigate(`/activities/:username`)}>
               My Activities
@@ -225,7 +174,6 @@ export default function MyPage() {
           </div>
         </div>
       </div>
-      <button onClick={signoutHandler}>Signout</button>
     </div>
   );
 }
