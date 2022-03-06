2e4ab86ed2f98d2df428322b8f21ff71245e68cc
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-03-06 11:00:19 +0400

    [Delete] 사용 안하는 리듀서 삭제

--- client/src/redux/modules/rootReducer.js
+++ client/src/redux/modules/rootReducer.js
@@ -1,16 +1,12 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import columns from "./columns";
-import columnRankings from "./columnRankings";
 import debates from "./debates";
-import debateRankings from "./debateRankings";
 
 const rootReducer = combineReducers({
   user,
   columns,
-  columnRankings,
   debates,
-  debateRankings,
 });
 
 export default rootReducer;
