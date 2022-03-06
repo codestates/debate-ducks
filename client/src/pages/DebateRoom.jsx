27974ddc478af6b4dbe810dfb194827941a9c10f
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-03-06 16:15:29 +0400

    [Update] DebateRoom 마무리
    - s3에 비디오 업로드 시 서버에 url, status, ended_at 업데이트
    - DebateRoom 접근 및 비디오 업로드 관련 controller 작성
    - 올바르지 않은 접근 시 에러 페이지로 리다이렉트

--- client/src/pages/DebateRoom.jsx
+++ client/src/pages/DebateRoom.jsx
@@ -1,15 +1,50 @@
 import io from "socket.io-client";
+import axios from "axios";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
 import RealtimeDebate from "../components/debate_room/RealtimeDebate";
+import Loading from "../components/Loading";
 
 export default function DebateRoom() {
+  const navigate = useNavigate();
   const { debateId } = useParams();
 
+  const userId = useSelector((state) => state.user.data.id);
+
   const [socket, setSocket] = useState({});
+  const [debateInfo, setDebateInfo] = useState({});
+  const [isPro, setIsPro] = useState(null);
+
   useEffect(() => {
     setSocket(io(`${process.env.REACT_APP_API_URL}`));
+
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/debate/debate_room/${debateId}`, { withCredentials: true })
+      .then((res) => {
+        setDebateInfo(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        navigate("/error");
+      });
   }, []);
 
-  return <div>{Object.keys(socket).length === 0 ? <div>Loading</div> : <RealtimeDebate socket={socket} debateId={debateId} />}</div>;
+  useEffect(() => {
+    if (userId === debateInfo.proId) {
+      setIsPro(true);
+    } else if (userId === debateInfo.conId) {
+      setIsPro(false);
+    }
+  }, [debateInfo.title]);
+
+  return (
+    <div>
+      {Object.keys(socket).length !== 0 && Object.keys(debateInfo).length !== 0 && isPro !== null ? (
+        <RealtimeDebate socket={socket} debateId={debateId} debateInfo={debateInfo} isPro={isPro} />
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
 }
--- server/routes/debate.js
+++ server/routes/debate.js
@@ -8,5 +8,7 @@ router.patch("/:debate_id", debateController.update_debate); // 토론 수정
 router.patch("/:debate_id/video", debateController.update_video); // 토론 비디오 업로드
 router.delete("/:debate_id", debateController.delete_debate); // 토론 삭제
 router.get("/list", debateController.get_debates); // 토론 리스트 조회
+router.get("/debate_room/:debate_id", debateController.get_debate_room); // 토론 방 조회
+router.patch("/debate_room/:debate_id/video", debateController.update_debate_room_video); // 토론 방에서 비디오 업로드
 
 module.exports = router;
