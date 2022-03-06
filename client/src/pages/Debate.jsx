0608af4fc40c4317f71dfaef3768caab1ffc9afb
strawberryoolongtea <strawberrycream@kakao.com>
2022-03-07 05:10:28 +0900

    [Fix] 디베이트 상태에 따른 화면 분기 및 참여 버튼 기능 수정

--- client/src/pages/Debate.jsx
+++ client/src/pages/Debate.jsx
@@ -6,6 +6,8 @@ import OnGoingDebate from "../components/debate/OnGoingDebate";
 import Voting from "../components/debate/Voting";
 import Completed from "../components/debate/Completed";
 import Loading from "../components/Loading";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+// import { divideColor } from "tailwindcss/defaultTheme";
 
 export default function Debate() {
   const [debate, setDebate] = useState({});
@@ -14,7 +16,9 @@ export default function Debate() {
 
   let { debateId } = useParams();
 
+  console.log("useEffect outer", debate);
   useEffect(() => {
+    console.log("useEffect inner", debate);
     axios.get(`${process.env.REACT_APP_API_URL}/debate/single/${debateId}`, { withCredentials: true }).then((data) => {
       const dayOfDeadline = data.data.data.debateInfo.ended_at;
       if (dayOfDeadline) {
@@ -26,8 +30,107 @@ export default function Debate() {
       setDebate(data.data.data);
       setIsLoading(false);
     });
-  }, []);
+  }, [debate?.prosProfile, debate?.consProfile]);
 
   console.log(deadline);
-  return <div>{isLoading ? <Loading /> : !debate?.debateInfo.video ? <OnGoingDebate {...debate} /> : deadline > 0 ? <Voting {...debate} /> : <Completed {...debate} />}</div>;
+  console.log("🔥🔥🔥🔥🔥🔥 its debate ", debate);
+  return (
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <div className="flex flex-col items-center">
+            <div className="w-screen h-410 relative">
+              <HiOutlineDotsVertical className="w-60 h-80 p-3 absolute top-0 right-0 text-white text-32 cursor-pointer z-20" />
+              <h1 className="font-poppins font-bold text-48 text-white z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{debate.debateInfo.title}</h1>
+              <div className="w-full h-410 absolute bg-ducks-blue-6667ab z-10 mix-blend-exclusion"></div>
+              <img className="object-cover object-center w-screen h-410" src={`/images/${debate.debateInfo.category.toLowerCase()}.jpg`} alt={debate.debateInfo.category} />
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            {!debate.debateInfo.participant_id ? (
+              debate.debateInfo.host_id === debate.debateInfo.pros_id ? (
+                <div className="w-410 flex justify-between items-center my-60 z-30">
+                  <div>
+                    <img src={debate.prosProfile} className="w-140 h-140 object-cover rounded-full mb-20 border-4 border-solid border-ducks-orange-ff9425" />
+                    <div className="text-center text-ducks-orange-ff9425 font-poppins font-bold">
+                      <div>pros</div>
+                      <div>{debate.prosName}</div>
+                    </div>
+                  </div>
+                  <div className="text-24 font-poppins font-bold">VS</div>
+                  <div>
+                    <div className="bg-grayduck w-140 h-140 bg-cover rounded-full mb-20"></div>
+                    <div className="text-center text-ducks-gray-ccc font-poppins">
+                      <div className="font-bold">cons</div>
+                      <div>not in</div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="w-410 flex justify-between items-center my-60 z-30">
+                  <div>
+                    <div className="bg-grayduck w-140 h-140 bg-cover rounded-full mb-20"></div>
+                    <div className="text-center text-ducks-gray-ccc font-poppins">
+                      <div className="font-bold">pros</div>
+                      <div>not in</div>
+                    </div>
+                  </div>
+                  <div className="text-24 font-poppins font-bold">VS</div>
+                  <div>
+                    <img src={debate.consProfile} className="w-140 h-140 object-cover rounded-full mb-20 border-4 border-solid border-ducks-blue-6667ab" />
+                    <div className="text-center text-ducks-blue-6667ab font-poppins font-bold">
+                      <div>cons</div>
+                      <div>{debate.consName}</div>
+                    </div>
+                  </div>
+                </div>
+              )
+            ) : (
+              <div className="w-410 flex justify-between items-center my-60 z-30">
+                <div>
+                  <img src={debate.prosProfile} className="w-140 h-140 object-cover rounded-full mb-20 border-4 border-solid border-ducks-orange-ff9425" />
+                  <div className="text-center text-ducks-orange-ff9425 font-poppins font-bold">
+                    <div>pros</div>
+                    <div>{debate.prosName}</div>
+                  </div>
+                </div>
+                <div className="text-24 font-poppins font-bold">VS</div>
+                <div>
+                  <img src={debate.consProfile} className="w-140 h-140 object-cover rounded-full mb-20 border-4 border-solid border-ducks-blue-6667ab" />
+                  <div className="text-center text-ducks-blue-6667ab font-poppins font-bold">
+                    <div>cons</div>
+                    <div>{debate.consName}</div>
+                  </div>
+                </div>
+              </div>
+            )}
+            {debate.debateInfo.status === "ongoing" ? (
+              <OnGoingDebate debate={debate} isLoading={isLoading} setDebate={setDebate} />
+            ) : debate.debateInfo.status === "voting" ? (
+              <Voting />
+            ) : debate.debateInfo.status === "completed" ? (
+              <Completed />
+            ) : null}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
+  // <>
+
+  //   <div>
+  //     {isLoading ? (
+  //       <Loading />
+  //     ) : debate.debateInfo.status === "ongoing" ? (
+  //       <OnGoingDebate {...debate} />
+  //     ) : debate.debateInfo.status === "voting" ? (
+  //       <Voting {...debate} />
+  //     ) : debate.debateInfo.status === "completed" ? (
+  //       <Completed {...debate} />
+  //     ) : null}
+  //   </div>
+  // </>
 }
