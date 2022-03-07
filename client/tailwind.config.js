9e3b0bb752d3ca25be639223d9794f31ed6b5bed
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-03-08 00:56:55 +0400

    [Fix] Tailwind CSS 커스텀 변수명과 디폴트 변수명 충돌

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         // padding, margin, width, height
         10: "0.625rem",
         12: "0.75rem",
-        14: "0.875rem",
+        13: "0.875rem",
         16: "1rem",
         18: "1.125rem",
         20: "1.25rem",
