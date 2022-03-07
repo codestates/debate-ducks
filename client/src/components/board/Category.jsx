1f91ce8a4524957728be2684b86c37737b6981e0
jennykim <jennykim97@gmail.com>
2022-03-07 16:49:23 +0900

    [Update] client측 코드 구조 개선

--- client/src/components/board/Category.jsx
+++ client/src/components/board/Category.jsx
@@ -1,4 +1,4 @@
-import { BlueBtn } from "../btn/BaseBtn";
+//import { BlueBtn } from "../btn/BaseBtn";
 import PropTypes from "prop-types";
 Category.propTypes = { categories: PropTypes.array, setCategories: PropTypes.func, list: PropTypes.array };
 
@@ -13,11 +13,23 @@ export default function Category({ categories, setCategories, list }) {
 
   return (
     <div className="w-960 mx-auto mb-60 flex flex-wrap justify-center">
-      {list.map((category, idx) => (
-        <BlueBtn key={idx} onClick={clickCategory} text={category}>
-          {/* {category} */}
-        </BlueBtn>
-      ))}
+      {list.map((category, idx) => {
+        //const selected = categories.find((item) => item === category);
+        const selected = categories.includes(category);
+        return (
+          <button
+            key={idx}
+            onClick={clickCategory}
+            className={
+              selected
+                ? "bg-ducks-blue-6667ab m-1 box-border rounded-16 h-32 px-24 capitalize text-14 font-poppins font-medium duration-200 ease-linear border border-ducks-blue-6667ab text-white hover:bg-ducks-blue-6667ab hover:text-white"
+                : "m-1 box-border rounded-16 h-32 px-24 capitalize text-14 font-poppins font-medium duration-200 ease-linear border border-ducks-blue-6667ab text-ducks-blue-6667ab hover:bg-ducks-blue-6667ab hover:text-white"
+            }
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 }
